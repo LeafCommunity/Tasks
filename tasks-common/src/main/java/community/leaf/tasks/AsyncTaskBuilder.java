b8d9a046fefe736c8dd6f5e9856b99a00d46197d
RezzedUp <RezzedUp@users.noreply.github.com>
2021-03-19 15:49:35 -0500

    Add LICENSE. And fix a minor thing.

--- tasks-common/src/main/java/community/leaf/tasks/AsyncTaskBuilder.java
+++ tasks-common/src/main/java/community/leaf/tasks/AsyncTaskBuilder.java
@@ -5,7 +5,7 @@ public class AsyncTaskBuilder extends TaskBuilder<AsyncTaskBuilder>
     public AsyncTaskBuilder(TaskScheduler<?> scheduler) { super(scheduler); }
     
     @Override
-    public Concurrency getConcurrency() { return Concurrency.ASYNC; }
+    public final Concurrency getConcurrency() { return Concurrency.ASYNC; }
     
     @Override
     public final boolean isAsync() { return true; }
