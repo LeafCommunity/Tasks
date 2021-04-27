3c849a136023e88c053bcdf48980ca655c55bcdb
RezzedUp <RezzedUp@users.noreply.github.com>
2021-04-27 13:28:48 -0500

    Swap license to MPL-2.0. Add license headers.

--- tasks-common/src/main/java/community/leaf/tasks/SyncTaskSource.java
+++ tasks-common/src/main/java/community/leaf/tasks/SyncTaskSource.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2021, RezzedUp <https://github.com/LeafCommunity/Tasks>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package community.leaf.tasks;
 
 public interface SyncTaskSource<B extends AbstractTaskBuilder<B, ? extends PendingMilliseconds<B>>>
