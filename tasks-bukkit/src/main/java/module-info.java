701eb7654a374ec97305574580bd9d9b3c01b315
RezzedUp <RezzedUp@users.noreply.github.com>
2021-04-28 11:09:58 -0500

    Modularize project.

---
+++ tasks-bukkit/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2021, RezzedUp <https://github.com/LeafCommunity/Tasks>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+module community.leaf.tasks.bukkit
+{
+    requires transitive community.leaf.tasks.minecraft;
+    requires static pl.tlinkowski.annotation.basic;
+    requires org.bukkit;
+    
+    exports community.leaf.tasks.bukkit;
+}
---
+++ tasks-minecraft/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2021, RezzedUp <https://github.com/LeafCommunity/Tasks>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+module community.leaf.tasks.minecraft
+{
+    requires transitive community.leaf.tasks;
+    requires static pl.tlinkowski.annotation.basic;
+    
+    exports community.leaf.tasks.minecraft;
+}
