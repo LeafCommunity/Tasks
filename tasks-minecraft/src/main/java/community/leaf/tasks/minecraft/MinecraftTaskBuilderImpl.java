361b44e818e7fdbb91e464bf2f70e7ba92871fe8
RezzedUp <RezzedUp@users.noreply.github.com>
2024-01-01 17:02:47 -0600

    Update license headers (2024).

--- tasks-minecraft/src/main/java/community/leaf/tasks/minecraft/MinecraftTaskBuilderImpl.java
+++ tasks-minecraft/src/main/java/community/leaf/tasks/minecraft/MinecraftTaskBuilderImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2021-2022, RezzedUp <https://github.com/LeafCommunity/Tasks>
+ * Copyright © 2021-2024, RezzedUp <https://github.com/LeafCommunity/Tasks>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
